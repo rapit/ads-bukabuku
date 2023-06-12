aa8b87312362bda9c9dc8dea7996b6953dbcabdf
Rafif Idul Fitra <96464544+rapit@users.noreply.github.com>
2023-06-12 16:21:06 +0700

    Add files via upload

---
+++ BukaBuku/src/Components/Navbar/Menu.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export const Menu = [
+  {
+    title : "Home",
+    url : "/",
+    cName : "nav-links",
+    icon : "fa-solid fa-house"
+  },
+  {
+    title : "About",
+    url : "About",
+    cName : "nav-links",
+    icon : "fa-solid fa-circle-info"
+  },
+  {
+    title : "Profile",
+    url : "Profile",
+    cName : "nav-links",
+    icon : "fa-solid fa-user",
+  },
+  {
+    title : "Login",
+    url : "Login",
+    cName : "nav-links-mobile",
+  }
+]
