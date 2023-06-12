743d44fbc75f65e2c8a39579dd110b1194b12fc3
Rafif Idul Fitra <96464544+rapit@users.noreply.github.com>
2023-06-12 16:35:48 +0700

    App.js

--- BukaBuku/src/App.js
+++ BukaBuku/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import Comments from "./Pages/Comments/Comments";
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <div className="App">
+      <Comments />
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
       }
