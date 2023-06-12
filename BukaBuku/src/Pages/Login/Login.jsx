b00be9996ad210fe20e2d9a53932c593feeb25d6
Rafif Idul Fitra <96464544+rapit@users.noreply.github.com>
2023-06-12 15:37:09 +0700

    Login.jsx

--- BukaBuku/src/Pages/Login/Login.jsx
+++ BukaBuku/src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './Login.css';
 import { Register } from "../Register/Register";
 import { Link } from 'react-router-dom';
+import Navbar from '../../Components/Navbar/Navbar';
 
 export const Login = (props) => {
   const [email, setEmail] = useState('');
@@ -28,12 +29,13 @@ export const Login = (props) => {
   return (
     <div className="login-header-container">
       <div className="auth-form-container">
+        <Navbar />  
         <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
-          <label htmlFor="email"></label>
-          <input className="kotak" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-          <label htmlFor="password"></label>
-          <input className="kotak" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+          <label htmlFor="email">Email :</label>
+          <input className="kotak" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Masukan Email" id="email" name="email" />
+          <label htmlFor="password">Password :</label>
+          <input className="kotak" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Masukan Password" id="password" name="password" />
           <button type="submit">Log In</button>
           </form>
       <Link to="/register" className='link-btn'>Don't have an account? Register here.</Link>
